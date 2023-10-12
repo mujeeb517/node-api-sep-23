9419a001918d5c0c370f5a83160dfa8ebedbe051
Mujib <mujib517@icloud.com>
2023-10-12 08:42:25 +0530

    handling different routes

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const http = require('http');
-
+const fs = require('fs');
 // call back
 // routing
+
+const products = [
+    { id: 1, brand: 'Apple', model: 'iPhone 13', price: 600, inStock: true },
+    { id: 2, brand: 'Apple', model: 'iPhone 14', price: 700, inStock: false },
+    { id: 3, brand: 'Apple', model: 'iPhone 15', price: 800, inStock: true },
+];
+
+// serialization: RAM->transfer, save
+// deserialization: json -> variables
 function handler(req, res) {
     switch (req.url) {
+        // www.google.com/about.html
         case '/':
             res.write('Hello Nodejs');
             res.end();
             break;
 
         case '/products':
-            res.write('Products');
+            res.write(JSON.stringify(products));
             res.end();
             break;
 
         case '/home':
-            res.write('home');
+            const contents = fs.readFileSync('index.html');
+            res.write(contents);
+            res.end();
+            break;
+
+        default:
+            res.write('Page you are looking for is not found');
             res.end();
             break;
     }
@@ -37,3 +53,6 @@ server.listen(port, cb);
 // localhost
 // testing
 // 192.38.49.59 -> 187.78.85.20
+// http server
+// tcp port
+// listens
